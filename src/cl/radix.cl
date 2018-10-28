0c0201b51b45d9b2678b375fa7b10c2256dca0b9
Dmitrii Kravchenko <equivalence1@gmail.com>
2018-10-29 01:19:59 +0300

    radix sort done

--- src/cl/radix.cl
+++ src/cl/radix.cl
@@ -1,4 +1,91 @@
-__kernel void radix(__global unsigned int* as)
+#ifdef __CLION_IDE__
+#include <libgpu/opencl/cl/clion_defines.cl>
+#endif
+
+#define unsigned int uint
+#define SWAP(a,b) {__local uint* tmp=a; a=b; b=tmp;}
+
+__kernel void count(__global uint* as,
+                    __global uint* cs,
+                    uint n,
+                    uint mask_offset,
+                    uint mask_width)
 {
-    // TODO
+  uint gid = get_global_id(0);
+  uint mask = ((1UL << mask_width) - 1) << mask_offset;
+  uint n_vals = 1 << mask_width;
+
+  if (gid < n) {
+    uint x = (as[gid] & mask) >> mask_offset;
+    for (uint i = 0; i < n_vals; i++)
+      cs[i * n + gid] = x == i ? 1 : 0;
+  }
+}
+
+/*
+ * Почему-то у меня не получается передать этот define как параметр
+ * (см объявление ocl::Kernel scan), поэтому просто объявляю его тут.
+ */
+#define BLOCK_SIZE 128
+
+__kernel void scan(__global uint* as,
+                   __global uint* bs,
+                   __global uint* cs,
+                   uint n,
+                   int zero_bs)
+{
+  uint gid = get_global_id(0);
+  uint lid = get_local_id(0);
+  uint block_size = get_local_size(0);
+  uint gr_id = get_group_id(0);
+
+  __local uint local_a[BLOCK_SIZE];
+  __local uint local_b[BLOCK_SIZE];
+  __local uint* a = local_a;
+  __local uint* b = local_b;
+
+  // init locals
+  if (gid < n) {
+    a[lid] = as[gid];
+    if (lid == 0) {
+      if (!zero_bs)
+        a[0] += bs[gr_id];
+    }
+  } else
+    a[lid] = 0;
+  barrier(CLK_LOCAL_MEM_FENCE);
+
+  // hillis_steele
+  for (uint s = 1; s < block_size; s <<= 1) {
+    if (lid > (s - 1))
+      b[lid] = a[lid] + a[lid - s];
+    else
+      b[lid] = a[lid];
+    barrier(CLK_LOCAL_MEM_FENCE);
+    SWAP(a, b);
+  }
+
+  // move to global
+  if (gid < n) {
+    cs[gid] = a[lid];
+    if (lid == 0)
+      bs[gr_id + 1] = a[block_size - 1];
+  }
+}
+
+__kernel void reorder(__global uint* as,
+                      __global uint* os,
+                      __global uint* bs,
+                      uint n,
+                      uint mask_offset,
+                      uint mask_width)
+{
+  uint gid = get_global_id(0);
+  uint mask = ((1UL << mask_width) - 1) << mask_offset;
+  uint n_vals = 1 << mask_width;
+
+  if (gid < n) {
+    uint x = (as[gid] & mask) >> mask_offset;
+    bs[os[x * n + gid] - 1] = as[gid];
+  }
 }
--- src/main_radix.cpp
+++ src/main_radix.cpp
@@ -23,6 +23,77 @@ void raiseFail(const T &a, const T &b, std::string message, std::string filename
 
 #define EXPECT_THE_SAME(a, b, message) raiseFail(a, b, message, __FILE__, __LINE__)
 
+#define BLOCK_SIZE 128
+
+void print_buff(gpu::gpu_mem_32u& as_gpu, unsigned int n)
+{
+    std::cout << "printing buffer:\n";
+    std::vector<unsigned int> a(n);
+    as_gpu.readN(a.data(), n);
+    for (int i = 0; i < (int)n; i++) {
+        std::cout << i << ": " << a[i] << std::endl;
+    }
+}
+
+/*
+ * since shared_device_buffer's copy constructor only
+ * does shallow copy, it's okay to return by value
+ */
+gpu::gpu_mem_32u prefix_sum(ocl::Kernel& scan,
+                            gpu::gpu_mem_32u& as_gpu,
+                            unsigned int n,
+                            unsigned int wg_size)
+{
+    unsigned int res_n = (n + wg_size - 1) / wg_size * wg_size;
+
+    gpu::gpu_mem_32u sums;
+    sums.resizeN(res_n);
+    gpu::gpu_mem_32u b_sums;
+    b_sums.resizeN(res_n / wg_size + 1);
+    {
+        // it's important to set first element to zero
+        unsigned int z = 0;
+        b_sums.writeN(&z, 1);
+    }
+
+    scan.exec(gpu::WorkSize(wg_size, n),
+              as_gpu, b_sums, sums, n, 1);
+
+    if (wg_size >= n)
+        return sums;
+
+    b_sums = prefix_sum(scan, b_sums, res_n / wg_size + 1, wg_size);
+    scan.exec(gpu::WorkSize(wg_size, n),
+              as_gpu, b_sums, sums, n, 0);
+
+    return sums;
+}
+
+void radix(gpu::gpu_mem_32u& as_gpu,
+           unsigned int n,
+           ocl::Kernel& count,
+           ocl::Kernel& scan,
+           ocl::Kernel& reorder)
+{
+    unsigned int mask_width = 2;
+
+    gpu::gpu_mem_32u counts_gpu;
+    counts_gpu.resizeN(n * (1 << mask_width));
+    gpu::gpu_mem_32u bs_gpu;
+    bs_gpu.resizeN(n);
+
+    unsigned int wg_size = BLOCK_SIZE;
+    unsigned int gw_size = n;
+
+    for (int i = 0; i < 32; i += mask_width) {
+        count.exec(gpu::WorkSize(wg_size, gw_size),
+                   as_gpu, counts_gpu, n, i, mask_width);
+        gpu::gpu_mem_32u s = prefix_sum(scan, counts_gpu, n * (1 << mask_width), wg_size);
+        reorder.exec(gpu::WorkSize(wg_size, gw_size),
+                     as_gpu, s, bs_gpu, n, i, mask_width);
+        as_gpu.swap(bs_gpu);
+    }
+}
 
 int main(int argc, char **argv)
 {
@@ -46,30 +117,31 @@ int main(int argc, char **argv)
         timer t;
         for (int iter = 0; iter < benchmarkingIters; ++iter) {
             cpu_sorted = as;
+            t.restart(); // Don't count vector's copy constructor time
             std::sort(cpu_sorted.begin(), cpu_sorted.end());
             t.nextLap();
         }
         std::cout << "CPU: " << t.lapAvg() << "+-" << t.lapStd() << " s" << std::endl;
         std::cout << "CPU: " << (n/1000/1000) / t.lapAvg() << " millions/s" << std::endl;
     }
-/*
+
     gpu::gpu_mem_32u as_gpu;
     as_gpu.resizeN(n);
 
-    {
-        ocl::Kernel radix(radix_kernel, radix_kernel_length, "radix");
-        radix.compile();
+    ocl::Kernel count(radix_kernel, radix_kernel_length, "count");
+    count.compile();
+    ocl::Kernel scan(radix_kernel, radix_kernel_length, "scan", "-DBLOCK_SIZE=" + std::to_string(BLOCK_SIZE));
+    scan.compile();
+    ocl::Kernel reorder(radix_kernel, radix_kernel_length, "reorder");
+    reorder.compile();
 
+    {
         timer t;
         for (int iter = 0; iter < benchmarkingIters; ++iter) {
             as_gpu.writeN(as.data(), n);
 
             t.restart(); // Запускаем секундомер после прогрузки данных чтобы замерять время работы кернела, а не трансфер данных
-
-            unsigned int workGroupSize = 128;
-            unsigned int global_work_size = (n + workGroupSize - 1) / workGroupSize * workGroupSize;
-            radix.exec(gpu::WorkSize(workGroupSize, global_work_size),
-                       as_gpu, n);
+            radix(as_gpu, n, count, scan, reorder);
             t.nextLap();
         }
         std::cout << "GPU: " << t.lapAvg() << "+-" << t.lapStd() << " s" << std::endl;
@@ -82,6 +154,6 @@ int main(int argc, char **argv)
     for (int i = 0; i < n; ++i) {
         EXPECT_THE_SAME(as[i], cpu_sorted[i], "GPU results should be equal to CPU results!");
     }
-*/
+
     return 0;
 }
