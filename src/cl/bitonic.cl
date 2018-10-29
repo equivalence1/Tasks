6e5f06913fd2b599d60978319a03a85313720a05
Dmitrii Kravchenko <equivalence1@gmail.com>
2018-10-29 03:58:23 +0300

    bitonic sort done

--- src/cl/bitonic.cl
+++ src/cl/bitonic.cl
@@ -1,4 +1,60 @@
-__kernel void bitonic(__global float* as)
+#ifdef __CLION_IDE__
+#include <libgpu/opencl/cl/clion_defines.cl>
+#endif
+
+#define unsigned int uint;
+
+__kernel void bitonic_large(__global float* as,
+                            uint n,
+                            uint bsize,
+                            uint skip_size
+)
 {
-    // TODO
+  uint gid = get_global_id(0);
+  uint up = gid % (2 * bsize) < bsize;
+
+  if (gid % (2 * skip_size) < skip_size && gid + skip_size < n) {
+    float a = as[gid];
+    float b = as[gid + skip_size];
+    if (up ^ (a <= b)) {
+      as[gid] = b;
+      as[gid + skip_size] = a;
+    }
+  }
+}
+
+// same as in radix sort
+#define BLOCK_SIZE 128
+
+__kernel void bitonic_small(__global float* as,
+                            uint n,
+                            uint bsize,
+                            uint skip_size
+)
+{
+  uint gid = get_global_id(0);
+  uint lid = get_local_id(0);
+  uint up = gid % (2 * bsize) < bsize;
+
+  __local float la[BLOCK_SIZE];
+
+  if (gid < n)
+    la[lid] = as[gid];
+
+  barrier(CLK_LOCAL_MEM_FENCE);
+
+  for (uint s = skip_size; s > 0; s /= 2) {
+    if (lid % (2 * s) < s && gid + s < n) {
+      float a = la[lid];
+      float b = la[lid + s];
+      if (up ^ (a <= b)) {
+        la[lid] = b;
+        la[lid + s] = a;
+      }
+    }
+    barrier(CLK_LOCAL_MEM_FENCE);
+  }
+
+  if (gid < n)
+    as[gid] = la[lid];
 }
--- src/main_bitonic.cpp
+++ src/main_bitonic.cpp
@@ -23,6 +23,36 @@ void raiseFail(const T &a, const T &b, std::string message, std::string filename
 
 #define EXPECT_THE_SAME(a, b, message) raiseFail(a, b, message, __FILE__, __LINE__)
 
+#define BLOCK_SIZE 128
+
+void print_buff(gpu::gpu_mem_32f& as_gpu, unsigned int n)
+{
+    std::cout << "printing buffer:\n";
+    std::vector<float> a(n);
+    as_gpu.readN(a.data(), n);
+    for (int i = 0; i < (int)n; i++) {
+        std::cout << i << ": " << a[i] << std::endl;
+    }
+}
+
+void bitonic(ocl::Kernel& bitonic_large,
+             ocl::Kernel& bitonic_small,
+             gpu::gpu_mem_32f& as_gpu,
+             unsigned int n)
+{
+    unsigned int wg_size = BLOCK_SIZE;
+    unsigned int gw_size = (n + wg_size - 1) / wg_size * wg_size;
+
+    for (unsigned int bsize = 2; bsize / 2 < n; bsize *= 2) {
+        unsigned int skip = bsize / 2;
+        for (; skip * 2 > wg_size; skip /= 2) {
+            bitonic_large.exec(gpu::WorkSize(wg_size, gw_size),
+                               as_gpu, n, bsize, skip);
+        }
+        bitonic_small.exec(gpu::WorkSize(wg_size, gw_size),
+                           as_gpu, n, bsize, skip);
+    }
+}
 
 int main(int argc, char **argv)
 {
@@ -52,24 +82,22 @@ int main(int argc, char **argv)
         std::cout << "CPU: " << t.lapAvg() << "+-" << t.lapStd() << " s" << std::endl;
         std::cout << "CPU: " << (n/1000/1000) / t.lapAvg() << " millions/s" << std::endl;
     }
-/*
+
     gpu::gpu_mem_32f as_gpu;
     as_gpu.resizeN(n);
 
     {
-        ocl::Kernel bitonic(bitonic_kernel, bitonic_kernel_length, "bitonic");
-        bitonic.compile();
+        ocl::Kernel bitonic_large(bitonic_kernel, bitonic_kernel_length, "bitonic_large");
+        bitonic_large.compile();
+        ocl::Kernel bitonic_small(bitonic_kernel, bitonic_kernel_length, "bitonic_small", "-DBLOCK_SIZE=" + std::to_string(BLOCK_SIZE));
+        bitonic_small.compile();
 
         timer t;
         for (int iter = 0; iter < benchmarkingIters; ++iter) {
             as_gpu.writeN(as.data(), n);
 
             t.restart(); // Запускаем секундомер после прогрузки данных чтобы замерять время работы кернела, а не трансфер данных
-
-            unsigned int workGroupSize = 128;
-            unsigned int global_work_size = (n + workGroupSize - 1) / workGroupSize * workGroupSize;
-            bitonic.exec(gpu::WorkSize(workGroupSize, global_work_size),
-                         as_gpu, n);
+            bitonic(bitonic_large, bitonic_small, as_gpu, n);
             t.nextLap();
         }
         std::cout << "GPU: " << t.lapAvg() << "+-" << t.lapStd() << " s" << std::endl;
@@ -82,6 +110,6 @@ int main(int argc, char **argv)
     for (int i = 0; i < n; ++i) {
         EXPECT_THE_SAME(as[i], cpu_sorted[i], "GPU results should be equal to CPU results!");
     }
-*/
+
     return 0;
 }
