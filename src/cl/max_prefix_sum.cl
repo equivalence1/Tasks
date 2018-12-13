f89b360dd40df4d85caaa4142cf9af38110ccb1f
Dmitrii Kravchenko <equivalence1@gmail.com>
2018-12-13 17:53:59 +0300

    max prefix sum done

--- src/cl/max_prefix_sum.cl
+++ src/cl/max_prefix_sum.cl
@@ -1,1 +1,67 @@
-// TODO
+#ifdef __CLION_IDE__
+#include <libgpu/opencl/cl/clion_defines.cl>
+#endif
+
+#define unsigned int uint
+#define SWAP(a,b) {__local uint* tmp=a; a=b; b=tmp;}
+
+#define BLOCK_SIZE 128
+
+__kernel void scan(__global int* as,
+                   __global int* bs,
+                   __global int* cs,
+                   uint n,
+                   int zero_bs)
+{
+  uint gid = get_global_id(0);
+  uint lid = get_local_id(0);
+  uint block_size = get_local_size(0);
+  uint gr_id = get_group_id(0);
+
+  __local int local_a[BLOCK_SIZE];
+  __local int local_b[BLOCK_SIZE];
+  __local int* a = local_a;
+  __local int* b = local_b;
+
+  // init locals
+  if (gid < n) {
+    a[lid] = as[gid];
+    if (lid == 0) {
+      if (!zero_bs)
+        a[0] += bs[gr_id];
+    }
+  } else
+    a[lid] = 0;
+
+  barrier(CLK_LOCAL_MEM_FENCE);
+
+  // hillis_steele
+  for (uint s = 1; s < block_size; s <<= 1) {
+    if (lid > (s - 1))
+      b[lid] = a[lid] + a[lid - s];
+    else
+      b[lid] = a[lid];
+    barrier(CLK_LOCAL_MEM_FENCE);
+    SWAP(a, b);
+  }
+
+  // move to global
+  if (gid < n) {
+    cs[gid] = a[lid];
+    if (lid == 0)
+      bs[gr_id + 1] = a[block_size - 1];
+  }
+}
+
+__kernel void max_scan(__global int* as,
+                       __global int* bs,
+                       uint n) {
+  int i = get_global_id(0);
+
+  if (2 * i + 1 < n) {
+    if (as[bs[2 * i]] >= as[bs[2 * i + 1]])
+      bs[i] = bs[2 * i];
+    else
+      bs[i] = bs[2 * i + 1];
+  }
+}
--- src/main_max_prefix_sum.cpp
+++ src/main_max_prefix_sum.cpp
@@ -1,7 +1,83 @@
 #include <libutils/misc.h>
 #include <libutils/timer.h>
 #include <libutils/fast_random.h>
+#include <libgpu/context.h>
+#include <libgpu/shared_device_buffer.h>
 
+#include <utility>
+#include "cl/max_prefix_sum_cl.h"
+
+#define BLOCK_SIZE 128
+
+/*
+ * since shared_device_buffer's copy constructor only
+ * does shallow copy, it's okay to return by value
+ */
+gpu::gpu_mem_32i prefix_sum(ocl::Kernel& scan,
+                            gpu::gpu_mem_32i& as_gpu,
+                            unsigned int n,
+                            unsigned int wg_size)
+{
+    unsigned int res_n = (n + wg_size - 1) / wg_size * wg_size;
+
+    gpu::gpu_mem_32i sums;
+    sums.resizeN(res_n);
+    gpu::gpu_mem_32i b_sums;
+    b_sums.resizeN(res_n / wg_size + 1);
+    {
+        // it's important to set first element to zero
+        int z = 0;
+        b_sums.writeN(&z, 1);
+    }
+
+    scan.exec(gpu::WorkSize(wg_size, n),
+              as_gpu, b_sums, sums, n, 1);
+
+    if (wg_size >= n)
+        return sums;
+
+    b_sums = prefix_sum(scan, b_sums, res_n / wg_size + 1, wg_size);
+    scan.exec(gpu::WorkSize(wg_size, n),
+              as_gpu, b_sums, sums, n, 0);
+
+    return sums;
+}
+
+gpu::gpu_mem_32i max_elem(ocl::Kernel& mx,
+                          gpu::gpu_mem_32i& as_gpu,
+                          gpu::gpu_mem_32i& maxes,
+                          unsigned int n) {
+    if (n == 1)
+        return maxes;
+
+    mx.exec(gpu::WorkSize(BLOCK_SIZE, (n + BLOCK_SIZE - 1) / BLOCK_SIZE * BLOCK_SIZE),
+              as_gpu, maxes, n);
+
+    return max_elem(mx, as_gpu, maxes, n / 2);
+}
+
+std::pair<int, int> max_prefix_sum(ocl::Kernel& scan,
+                                   ocl::Kernel& mx,
+                                   gpu::gpu_mem_32i& as_gpu,
+                                   unsigned int n) {
+    auto sums_gpu = prefix_sum(scan, as_gpu, n, BLOCK_SIZE);
+    std::vector<int> sums_cpu(n);
+    sums_gpu.read(sums_cpu.data(), sizeof(int) * n, 0);
+
+    std::vector<int> maxes_cpu(n);
+    for (int i = 0; i < n; i++) {
+        maxes_cpu[i] = i;
+    }
+    gpu::gpu_mem_32i maxes;
+    maxes.resize(n * sizeof(int));
+    maxes.write(maxes_cpu.data(), n * sizeof(int));
+
+    auto gpu_res = max_elem(mx, sums_gpu, maxes, n);
+    std::vector<int> cpu_res(1);
+    gpu_res.read(cpu_res.data(), sizeof(int), 0);
+
+    return std::make_pair(sums_cpu[cpu_res[0]], cpu_res[0]);
+}
 
 template<typename T>
 void raiseFail(const T &a, const T &b, std::string message, std::string filename, int line)
@@ -71,7 +147,31 @@ int main(int argc, char **argv)
         }
 
         {
-            // TODO: implement on OpenCL
+            gpu::Device device = gpu::chooseGPUDevice(argc, argv);
+            gpu::Context context;
+            context.init(device.device_id_opencl);
+            context.activate();
+
+            ocl::Kernel scan(max_prefix_sum_kernel, max_prefix_sum_kernel_length, "scan");
+            scan.compile();
+
+            ocl::Kernel mx(max_prefix_sum_kernel, max_prefix_sum_kernel_length, "max_scan");
+            mx.compile();
+
+            as.insert(as.begin(), 0);
+            gpu::gpu_mem_32i as_gpu;
+            as_gpu.resize(n * sizeof(as[0]));
+            as_gpu.write(as.data(), n * sizeof(as[0]));
+
+            timer t;
+            for (int iter = 0; iter < benchmarkingIters; ++iter) {
+                auto res = max_prefix_sum(scan, mx, as_gpu, (unsigned int)n);
+                EXPECT_THE_SAME(reference_max_sum, res.first, "GPU result should be consistent!");
+                EXPECT_THE_SAME(reference_result, res.second, "GPU result should be consistent!");
+                t.nextLap();
+            }
+            std::cout << "GPU: " << t.lapAvg() << "+-" << t.lapStd() << " s" << std::endl;
+            std::cout << "GPU: " << (n / 1000.0 / 1000.0) / t.lapAvg() << " millions/s" << std::endl;
         }
     }
 }
